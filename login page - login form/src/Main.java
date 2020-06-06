544642c13395c4f1b19f86b0636a8015d36173bf
jsarr11 <65488351+jsarr11@users.noreply.github.com>
2020-06-07 01:17:01 +0300

    Add files via upload
    added signup form

---
+++ login page - login form/src/Main.java
@@ -0,0 +1,23 @@
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+        primaryStage.setTitle("TrackTruck");
+        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
